9ba505c7c1f3edc689b88a8aa1792d3441177f40
echeverriaalex <aleex.naahuel@outlook.com>
2024-12-01 10:41:01 -0300

    cambio icono, modifico mensajes de hero y otras modificaciones en fotter y el form y retoques de estilo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //import './style.css'
 import './assets/styles.css'
+import './assets/mediaqueries.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
@@ -34,11 +35,34 @@ setupCounter(document.querySelector('#counter'))
 */
 
 
-const result = await fetch('https://dummyjson.com/products?limit=0')
+const UrlAllProducts = 'https://dummyjson.com/products?limit=0'
+const producstCatalog = document.querySelector('#products-catalog');
+
+
+const getAllProducts = async(url)=>{
+  let result = await fetch(url)
+    .then(res => {
+        if(!res.ok){
+          console.log('API Error');
+          return
+        }
+        return res.json()
+      }
+    )
+    .then(data => {
+      return data.products;
+    })
+  
+  console.log("result dentro de la funcion --> " + result);
+  
+}
+
+
+/*
+const result = await fetch(UrlAllProducts)
 .then(res => {
   if(!res.ok){
     console.log('API Error');
-    
   }
     return res.json()
   }
@@ -46,19 +70,21 @@ const result = await fetch('https://dummyjson.com/products?limit=0')
 .then(data => {
   return data.products;
 })
+*/
 
 const createProductTemplate = (product)=>{
   const {id, title, description, category, price, stock, tags, brand, 
   sku, meta, images, thumbnail} = product
   return `
   <div class="product-card">
-    <!-- <img src='${images[0]}' alt=""> -->
-    <img class='image-product' src='${thumbnail}' alt="">
+    <img class="product-image" src="${images[0]}" alt="Imagen del producto">
+    <!-- <img class="product-image" src="${thumbnail}" alt="Imagen del producto"> -->
     <p>${id}</p>
-    <h2>${title}</h2>
-    <p class='description'>${description}</p>
+    <h2 class="product-title">${title}</h2>
+    <p class="product-description">${description}</p>
+    <p class="product-price">$ ${price}</p>
     <div class="button-container">
-        <button class="btn-cart">Agregar a carrito</button>
+      <button class="btn-cart">Agregar a carrito</button>
     </div>
   </div>`
 }
@@ -72,14 +98,33 @@ const rederProducts = () =>{
 }
 */
 
-const rederProducts = () =>{
+const rederProducts = (result) =>{
   return result.map(createProductTemplate).join('')
 }
 
 
-console.log(rederProducts());
+// console.log(rederProducts());
 
 
-document.querySelector('#catalog-products').innerHTML = rederProducts()
 
 
+
+const init = () =>{
+  let productList = getAllProducts(UrlAllProducts)
+  
+  console.log("Mostrando productos --> " + productList);
+  
+
+
+
+  //producstCatalog.innerHTML = rederProducts(productList);
+
+
+
+  
+
+}
+
+
+
+init();
